4575b893e8c9f922348c8eafe31be2eed2d2b5d2
KUNWEI <kwei@thoughtworks.com>
2022-10-26 20:07:24 +0800

    test(Question 2): use SpyPrinter to spy print of a real Printer

---
+++ CashRegisterTest/SpyPrinter.cs
@@ -0,0 +1,15 @@
+namespace CashRegisterTest
+{
+	using CashRegister;
+
+	public class SpyPrinter : Printer
+	{
+		public bool HasPrinted { get; set; }
+		public override void Print(string content)
+		{
+			// send message to a real printer
+			base.Print(content);
+			HasPrinted = true;
+		}
+	}
+}
