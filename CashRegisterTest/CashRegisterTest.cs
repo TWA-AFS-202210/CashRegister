1c3c8b8a96e66a2a3a65100e475348793b4e45d5
KUNWEI <kwei@thoughtworks.com>
2022-10-26 15:43:33 +0800

    test(question4): create test for stub printer

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -18,5 +18,18 @@ namespace CashRegisterTest
 			Assert.True(printer.HasPrinted);
 			Assert.Equal("stub content", printer.PrintContent);
 		}
+
+		[Fact]
+		public void Should_throw_HardwareException_when_process_given_stub_printer_throw_out_of_paper_exception()
+		{
+			// given
+			StubPrinter printer = new StubPrinter();
+			var cashRegister = new CashRegister(printer);
+			var purchase = new Purchase();
+			// when
+			// then
+			HardwareException hardwareException = Assert.Throws<HardwareException>(() => cashRegister.Process(purchase));
+			Assert.Equal("Printer is out of paper.", hardwareException.Message);
+		}
 	}
 }
