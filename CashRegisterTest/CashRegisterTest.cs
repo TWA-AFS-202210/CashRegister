ae1b729a1757bb7512a05957eb08ffae08bf0786
KUNWEI <kwei@thoughtworks.com>
2022-10-26 16:05:06 +0800

    test(question5): use Moq test cash register

--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -1,3 +1,5 @@
+using Moq;
+
 namespace CashRegisterTest
 {
 	using CashRegister;
@@ -30,6 +32,33 @@ namespace CashRegisterTest
 			// then
 			HardwareException hardwareException = Assert.Throws<HardwareException>(() => cashRegister.Process(purchase));
 			Assert.Equal("Printer is out of paper.", hardwareException.Message);
+		}
+
+		[Fact]
+		public void Should_print_call_when_run_process_given_spy_printer()
+		{
+			// Given
+			var printer = new Mock<Printer>();
+			var cashRegister = new CashRegister(printer.Object);
+			var purchase = new Purchase();
+			// when
+			cashRegister.Process(purchase);
+			// then
+			printer.Verify(_ => _.Print(It.IsAny<string>()));
+		}
+
+		[Fact]
+		public void Should_print_purchase_content_when_run_process_given_stub_purchase()
+		{
+			// Given
+			var spyPrinter = new Mock<Printer>();
+			var cashRegister = new CashRegister(spyPrinter.Object);
+			var stubPurchase = new Mock<Purchase>();
+			stubPurchase.Setup(x => x.AsString()).Returns("moq stub content");
+			// when
+			cashRegister.Process(stubPurchase.Object);
+			// then
+			spyPrinter.Verify(_ => _.Print("moq stub content"));
 		}
 	}
 }
